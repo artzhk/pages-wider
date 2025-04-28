7a44d3ed2b95cfba6b163431cd0485d1872493f8
artzhk <mr.zhukovsky02@gmail.com>
2025-04-28 06:41:56 +0200

    init

---
+++ background.js
@@ -0,0 +1,24 @@
+const wider = () => {
+  const elements = {};
+  [].forEach.call(document.querySelectorAll("div, main"), function (el) {
+    const currentWidth = window.getComputedStyle(el).maxWidth;
+    if (currentWidth != "100%") {
+      elements[el.id] = { initialWidth: currentWidth };
+      el.style.maxWidth = "100%";
+    }
+  });
+  return elements;
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "Wide",
+  });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: wider,
+  });
+});
