60232c0f2abfc53ab37ab84b96a2f86775c2f92c
Artem Zhukovskyi <mr.zhukovsky02@gmail.com>
2025-07-21 10:27:57 +0200

    feat: more tags handling, more websites coverage

--- background.js
+++ background.js
@@ -1,24 +1,34 @@
 const wider = () => {
-  const elements = {};
-  [].forEach.call(document.querySelectorAll("div, main"), function (el) {
-    const currentWidth = window.getComputedStyle(el).maxWidth;
-    if (currentWidth && currentWidth != "100%") {
-      elements[el.id] = { initialWidth: currentWidth };
-      el.style.maxWidth = "100%";
-    }
-  });
-  return elements;
+        [].forEach.call(document.querySelectorAll("div, main, table"), function (el) {
+                const maxWidth = window.getComputedStyle(el).maxWidth;
+                const width = window.getComputedStyle(el).width;
+		const _float = window.getComputedStyle(el).float;
+
+		const numericWidth = parseFloat(width);
+		const inReadFriendlyRange = numericWidth < 1000 && numericWidth > 600;
+
+		if (_float && _float !== "none") {
+			el.style.float = "none";
+		}
+                if (numericWidth != NaN && inReadFriendlyRange) {
+                        el.style.width = "auto";
+                }
+                if (maxWidth && maxWidth != "none") {
+                        el.style.maxWidth = "100%";
+                }
+        });
 };
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "Wide",
-  });
+        chrome.action.setBadgeText({
+                text: "Wide",
+        });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: wider,
-  });
+        await chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: wider,
+        });
 });
+
